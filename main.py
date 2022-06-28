d4f862eedee201dec048a101b7ee24569692fd59
Span4er <span4er59rus@mail.ru>
2022-06-28 13:07:55 +0500

    InterUp&FunctionImprove

--- main.py
+++ main.py
@@ -2,16 +2,9 @@ from scanner import stream_scan
 from ui import MyApp
 import cv2
 from glob import glob
-
-
-
-
-
-
-
+from DTMTRX import datamatrix, datamatrixTrue
 
 
 if __name__ == '__main__':
-    
+    #datamatrixTrue()
     MyApp().run()
-    
--- ui.py
+++ ui.py
@@ -1,10 +1,50 @@
-from scanner import stream_scan
-import kivy
+from scanner import stream_scan, decode
+from pyzbar import pyzbar
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import cv2
+import time
+import os
 
+
+class FirstScreen(Screen):
+    pass
+
+
+class SecondScreen(Screen):
+    pass
+
+
+class ReportScreen(Screen):
+    pass
+
+
+class CameraScreen(Screen):
+    def startScan(self):
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        name = "IMG_{}.png".format(timestr)
+        camera.export_to_png(name)#"IMG_{}.png".format(timestr))
+        frame = cv2.imread(name)
+        decoded_objects = pyzbar.decode(frame)
+        for obj in decoded_objects:
+            # draw the barcode
+            print(f"Обнаружен код:\n{obj}")
+            #image = draw_barcode(obj, image)
+            # print barcode type & data
+            print("Тип:", obj.type)
+            print("Данные:", obj.data)
+            print()
+        os.remove(name)
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file('stable.kv')
 
 
 class MyApp(App):
@@ -13,10 +53,13 @@ class MyApp(App):
     #bl.add_widget(Label(text ='Menu'))
     #bl.add_widget(Label(text = '_'))
     #bl.add_widget(Button(text = 'ok'))
-    return MyRoot()
+    #return MyRoot()
+    return kv
 
-class MyRoot(BoxLayout):
-    def __init__(self):
-        super(MyRoot, self).__init__()
-    def generate_number(self):
-        stream_scan()
+
+# class MyRoot(BoxLayout):
+#     def __init__(self):
+#         super(MyRoot, self).__init__()
+#
+#     def generate_number(self):
+#         stream_scan()

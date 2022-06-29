d02092a81aada7f22a3d5bcef45cc5d151dbf674
Vladislav _ <sophisticatedTrickery@outlook.com>
2022-06-30 00:07:37 +0500

    ui 0.2

--- ui.py
+++ ui.py
@@ -2,36 +2,37 @@ from scanner import stream_scan, decode
 from pyzbar import pyzbar
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 from kivy.lang import Builder
 import cv2
 import time
 import os
 
+Window.clearcolor = (208/255.0, 208/255.0, 208/255.0, 1)
 
-class FirstScreen(Screen):
+class WelcomeScreen(Screen):
     pass
 
 
-class SecondScreen(Screen):
-    pass
+# class SecondScreen(Screen):
+#     pass
 
 
 class ReportScreen(Screen):
     pass
 
 
-class CameraScreen(Screen):
+class ScanScreen(Screen):
     def startScan(self):
         camera = self.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
         name = "IMG_{}.png".format(timestr)
-        camera.export_to_png(name)#"IMG_{}.png".format(timestr))
+        camera.export_to_png(name)  # "IMG_{}.png".format(timestr))
         frame = cv2.imread(name)
         decoded_objects = pyzbar.decode(frame)
         for obj in decoded_objects:
-            # draw the barcode
             print(f"Обнаружен код:\n{obj}")
-            #image = draw_barcode(obj, image)
+            # image = draw_barcode(obj, image)
             # print barcode type & data
             print("Тип:", obj.type)
             print("Данные:", obj.data)
@@ -43,18 +44,12 @@ class WindowManager(ScreenManager):
     pass
 
 
-kv = Builder.load_file('stable.kv')
+kv = Builder.load_file('ui_main.kv')
 
 
 class MyApp(App):
-  def build(self):
-    #bl = BoxLayout(orientation = 'vertical')
-    #bl.add_widget(Label(text ='Menu'))
-    #bl.add_widget(Label(text = '_'))
-    #bl.add_widget(Button(text = 'ok'))
-    #return MyRoot()
-    return kv
-
+    def build(self):
+        return kv
 
 # class MyRoot(BoxLayout):
 #     def __init__(self):

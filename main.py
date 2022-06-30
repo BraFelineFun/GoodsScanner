2af9084243e2bb836c53d9a0f831465a21d39c43
Vladislav _ <sophisticatedTrickery@outlook.com>
2022-06-30 21:47:51 +0500

    ui 0.3 - Kivy MD

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from ui import MyApp
 import cv2
 from glob import glob
 from DTMTRX import datamatrix, datamatrixTrue
+from kivymd.font_definitions import theme_font_styles
 
 
 if __name__ == '__main__':
     #datamatrixTrue()
     MyApp().run()
+

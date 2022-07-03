99e6a81058632ae31be5f12fe286eb96da2215dc
Vladislav _ <sophisticatedTrickery@outlook.com>
2022-07-03 22:37:14 +0500

    report Function

--- ui.py
+++ ui.py
@@ -9,95 +9,101 @@ from kivy_garden import xcamera
 import cv2
 import time
 import os
+import json
 
 dataBase = {
     "b'5055856412670'": "Тип Штрих кода: EAN13\nСтрана производитель: Великобритания\nИгровой диск Dishonored 2",
     "b'0711719843948'": "Тип Штрих кода: EAN13\nСтрана производитель: США/Канада\nИгровой диск BloodBorne"}
 
+class UI:
+    class WelcomeScreen(Screen):
+        pass
 
-class WelcomeScreen(Screen):
-    pass
+    class ReportScreen(Screen):
+        productCode = ''
+        def createReport(self, objs):
+            text_objs = []
+            text_objs.append({"userName": objs[0].text})
+            text_objs.append({"product": objs[1].text})
+            text_objs.append({"product": self.productCode})
+            text_objs.append({"description": objs[2].text})
 
-
-class ReportScreen(Screen):
-    pass
-
-
-class ScanScreen(Screen):
-    def showData(self, obj):
-        value = dataBase.get(str(obj))
-        if value is not None:
-            dialog = MDDialog(text=str(value))
-            dialog.open()
-        else:
-            dialog = MDDialog(text=str(obj))
+            dialog = MDDialog(text=str(json.dumps(text_objs)))
             dialog.open()
 
-    def startScan(self):
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        name = "IMG_{}.png".format(timestr)
-        camera.export_to_png(name)  # "IMG_{}.png".format(timestr))
+    class ScanScreen(Screen):
+        def showData(self, obj):
+            value = dataBase.get(str(obj))
+            UI.ReportScreen.productCode = obj
 
-        if(platform == "android"):
-            frame = cv2.imread("/data/data/org.test.contafactapp/files/{}".format(name))
-        else:
-            frame = cv2.imread(name)
-        data = ""
-        decoded_objects = pyzbar.decode(frame)
-        for obj in decoded_objects:
-            print(f"Обнаружен код:\n{obj}")
-            # image = draw_barcode(obj, image)
-            # print barcode type & data
-            print("Тип:", obj.type)
-            print("Данные:", obj.data)
-            data = obj.data
-            print()
-        if(platform == "android"):
-            os.remove("/data/data/org.test.contafactapp/files/{}".format(name))
-        else:
-            os.remove(name)
+            if value is not None:
+                dialog = MDDialog(text=str(value))
+                dialog.open()
+            else:
+                dialog = MDDialog(text=str(obj))
+                dialog.open()
 
-        if data != "":
-            self.showData(str(data))
+        def startScan(self):
+            camera = self.ids['camera']
+            timestr = time.strftime("%Y%m%d_%H%M%S")
+            name = "IMG_{}.png".format(timestr)
+            camera.export_to_png(name)  # "IMG_{}.png".format(timestr))
 
-    def doScanMatrix(self):
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        name = "IMG_{}.png".format(timestr)
-        camera.export_to_png(name)  # "IMG_{}.png".format(timestr))
-        if platform =="android":
-            data = datamatrix("/data/data/org.test.contafactapp/files/{}".format(name))
-        else:
-            data = datamatrix(name)
-        self.showData((str(data)))
-        if (platform == "android"):
-            os.remove("/data/data/org.test.contafactapp/files/{}".format(name))
-        else:
-            os.remove(name)
+            if (platform == "android"):
+                frame = cv2.imread("/data/data/org.test.contafactapp/files/{}".format(name))
+            else:
+                frame = cv2.imread(name)
+            data = ""
+            decoded_objects = pyzbar.decode(frame)
+            for obj in decoded_objects:
+                print(f"Обнаружен код:\n{obj}")
+                # image = draw_barcode(obj, image)
+                # print barcode type & data
+                print("Тип:", obj.type)
+                print("Данные:", obj.data)
+                data = obj.data
+                print()
+            if (platform == "android"):
+                os.remove("/data/data/org.test.contafactapp/files/{}".format(name))
+            else:
+                os.remove(name)
 
+            if data != "":
+                self.showData(str(data))
 
-class WindowManager(ScreenManager):
-    pass
+        def doScanMatrix(self):
+            camera = self.ids['camera']
+            timestr = time.strftime("%Y%m%d_%H%M%S")
+            name = "IMG_{}.png".format(timestr)
+            camera.export_to_png(name)  # "IMG_{}.png".format(timestr))
+            if platform == "android":
+                data = datamatrix("/data/data/org.test.contafactapp/files/{}".format(name))
+            else:
+                data = datamatrix(name)
+            self.showData((str(data)))
+            if (platform == "android"):
+                os.remove("/data/data/org.test.contafactapp/files/{}".format(name))
+            else:
+                os.remove(name)
 
+    class WindowManager(ScreenManager):
+        pass
 
-class MyApp(MDApp):
+    class MyApp(MDApp):
 
-    def __init__(self, **kwargs):
-        self.title = "GoodsScanner"
-        super().__init__(**kwargs)
+        def __init__(self, **kwargs):
+            self.title = "GoodsScanner"
+            super().__init__(**kwargs)
 
+        def waitRequest(self):
+            from android.permissions import request_permissions, Permission
+            request_permissions(
+                [Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 
-    def waitRequest(self):
-        from android.permissions import request_permissions, Permission
-        request_permissions(
-            [Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
-
-    def build(self):
-        self.theme_cls.primary_palette = "Blue"
-        self.theme_cls.primary_hue = "500"
-        self.theme_cls.theme_style = "Dark"
-        if platform == "android":
-            self.waitRequest()
-        self.root = Builder.load_file('ui_main.kv')
-
+        def build(self):
+            self.theme_cls.primary_palette = "Blue"
+            self.theme_cls.primary_hue = "500"
+            self.theme_cls.theme_style = "Dark"
+            if platform == "android":
+                self.waitRequest()
+            self.root = Builder.load_file('ui_main.kv')
